2825214cc7b951739a776b26354e58d82cd73417
Sebastian Ferrer <ferrers1023@gmail.com>
2024-09-23 01:18:41 -0400

    Update code to working condition Still has bug of printing everything on a new line

---
+++ rangefinder.py
@@ -0,0 +1,23 @@
+userInput = input("Please enter in the range(s) you want printed: ")
+
+stringStart = ""
+stringEnd = ""
+
+userInput = userInput.split(" ")
+
+
+for i in userInput:
+    if "-" in i:
+        stringStart = i[0:i.index("-")]
+        if " " in i:
+            stringEnd = i[i.index("-")+1:i.index(" ")+1]
+        else:
+            stringEnd = i[i.index("-")+1:len(i)]
+
+        n = int(stringStart)
+        while n <= int(stringEnd):
+            print(str(n) +  " ")
+            n += 1
+    else:
+        print(i)
+            
